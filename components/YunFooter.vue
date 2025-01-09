fdbdb0d96e99edeb14a64e3f69f80fae868be48f
Zengzzhao <787332942@qq.com>
2025-01-09 21:31:41 +0800

    'update---'

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -12,12 +12,12 @@ onMounted(() => {
         fixed: true,
         order: 'random',
         audio: [
-        {
-            name: '酒馆儿',
-            artist: '蛋黄',
-            url: 'https://music.163.com/song/media/outer/url?id=1466888386.mp3',
-            cover: 'https://p2.music.126.net/yKVO0wAri_TPgq1ds12e2w==/109951165184677384.jpg?param=130y130',
-        }
+            {
+                name: '酒馆儿',
+                artist: '蛋黄',
+                url: 'https://music.163.com/song/media/outer/url?id=1466888386.mp3',
+                cover: 'https://p2.music.126.net/yKVO0wAri_TPgq1ds12e2w==/109951165184677384.jpg?param=130y130',
+            }
         ]
     });
 });
